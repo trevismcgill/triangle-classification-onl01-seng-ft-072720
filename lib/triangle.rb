ff818f67e3b2e47ebb11fd300890c953696fe92e
Trevis McGill <trevismcgill25@gmail.com>
2020-08-12 04:21:05 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :sides
+
+
+  def initialize(l1, l2, l3)
+    @sides = [l1, l2, l3]
+    # binding.pry
+  end
+
+def valid_tri?
+  ((@sides[0] + @sides[1] > @sides[2]) && (@sides[0] + @sides[2] > @sides[1]) && (@sides[1] + @sides[2] > @sides[0])) && @sides.all? {|side| side > 0}
+  # binding.pry
+end
+
+
+def kind()
+  # binding.pry
+  if valid_tri? != true
+    raise TriangleError
+  else
+    if valid_tri? == true && ((@sides[0] == @sides[1]) && (@sides[0] == @sides[2]))
+      :equilateral
+    elsif valid_tri? == true && ((@sides[0] == @sides [1]) || (@sides[0] == @sides [2]) || (@sides[1] == @sides [2]))
+      :isosceles
+    elsif valid_tri? == true && @sides.uniq == @sides
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+      # triangle error code
+
+end
+
 end
