ddb7dc981db10cba476adac1e747a8b7e4787483
Trevis McGill <trevismcgill25@gmail.com>
2020-08-12 03:45:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,14 +19,15 @@ def kind()
   # binding.pry
   if valid_tri? != true
     raise TriangleError
-  if valid_tri? == true && @sides.sum / 3 == @sides[0]
-   :equilateral
-elsif valid_tri? == true && ((@sides[0] == @sides [1]) || (@sides[0] == @sides [2]) || (@sides[1] == @sides [2]))
-  :isosceles
-elsif valid_tri? == true && @sides.uniq == true
-  :scalene
-end
-end
+  else
+    if valid_tri? == true && @sides.sum / 3 == @sides[0]
+      :equilateral
+    elsif valid_tri? == true && ((@sides[0] == @sides [1]) || (@sides[0] == @sides [2]) || (@sides[1] == @sides [2]))
+      :isosceles
+    elsif valid_tri? == true && @sides.uniq == true
+      :scalene
+    end
+  end
 end
 
 class TriangleError < StandardError
